d40c583b6f10984d36cdf01aef056e2e92aaad3e
Prateek sahu <prateeksahu956@gmail.com>
2023-04-22 00:03:28 +0530

    Time: 2318 ms (54.28%), Space: 46.8 MB (40.95%) - LeetHub

---
+++ profitable-schemes/profitable-schemes.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def profitableSchemes(self, n, minProfit, group, profit):
+        MOD = 10**9 + 7
+        length = len(group)
+        dp = [[[0] * (minProfit + 1) for _ in range(n + 1)] for _ in range(length + 1)]
+        dp[0][0][0] = 1
+        for i in range(1, length + 1):
+            members, earn = group[i - 1], profit[i - 1]
+            for j in range(n + 1):
+                for k in range(minProfit + 1):
+                    if j < members:
+                        dp[i][j][k] = dp[i - 1][j][k]
+                    else:
+                        dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][j - members][max(0, k - earn)]) % MOD
+        
+        total = sum(dp[length][j][minProfit] for j in range(n + 1))
+        return total % MOD
