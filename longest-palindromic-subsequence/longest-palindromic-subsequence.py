2e531e5cd70d25ab6ed5c42fc6e668f31b294ef3
Prateek sahu <prateeksahu956@gmail.com>
2023-04-22 15:03:18 +0530

    Time: 4890 ms (5.2%), Space: 528.3 MB (5.1%) - LeetHub

--- longest-palindromic-subsequence/longest-palindromic-subsequence.py
+++ longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -1,13 +1,18 @@
 class Solution(object):
+    @functools.lru_cache(None)
+    def check(self,i,j):
+        if i>=0 and i<self.n and j>=0 and j<self.m:
+            if self.a[i]==self.b[j]:
+                return self.check(i+1,j+1)+1
+            else:
+                a=self.check(i+1,j)
+                b=self.check(i,j+1)
+                return max(a,b)
+        return 0
     def longestPalindromeSubseq(self, s):
-        n=len(s)
-        mat=[[0]*(n) for i in range(n)]
-        for i in range(n-1,-1,-1):
-            mat[i][i]=1
-            for j in range(i+1,n):
-                if s[i]==s[j]:
-                    mat[i][j]=mat[i+1][j-1]+2
-                else:
-                    mat[i][j]=max(mat[i+1][j],mat[i][j-1])
-        return mat[0][-1]
+        self.a=s
+        self.b=s[::-1]
+        self.n=len(s)
+        self.m=len(s)
+        return self.check(0,0)
                 
